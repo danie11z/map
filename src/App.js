58d7bff5d0a69a5425c6952d79cdf95953873fdf
danie11z <daniel.estudando1@gmail.com>
2022-07-16 14:57:55 -0300

    Introducing map in JS React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class mapReact extends Component{
+  state = {
+    cadastro: [
+      {nome: 'Katara', idade: 14, naturalidade: 'Tribo da Água do Sul'},
+      {nome: 'Toph', idade: 12, naturalidade: 'Gaoling'},
+      {nome: 'Appa', idade: 106, naturalidade: 'Templo do Ar Oriental'},
+      {nome: 'Aang', idade: 112, naturalidade: 'Templo do Ar do Sul'},
+      {nome: 'Sokka', idade: 15, naturalidade: 'Tribo da Água do Sul'},
+      {nome: 'Zuko', idade: 16, naturalidade: 'Nação do Fogo'},
+    ]
+  };
+
+  render(){
+    return(
+      <section>
+        {this.state.cadastro.map((rastreador) => (
+          <div>
+            <p>
+              {rastreador.nome}, {rastreador.idade}, {rastreador.naturalidade}.
+            </p>
+          </div>
+        ))}
+      </section>
+    )
+  }
 }
-
-export default App;
